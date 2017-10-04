b31a680048bb5b20e6143ff29b098c75d26d16b7
fhossain615 <32169772+fhossain615@users.noreply.github.com>
2017-10-04 21:06:30 +0600

    Update TankThread.java

--- TankThread.java
+++ TankThread.java
@@ -10,13 +10,72 @@ package game;
  * @author Admin
  */
 public class TankThread extends Thread{
+     public static int possitionX;
+    public static int possitionY;
+    public static String direction;
+    
+    private static String[] rotate = {"up","down","left","right"};
+    Game gm =new Game();
 
     public TankThread(int possitionX,int possitionY, String direction) {
+        this.possitionX = possitionX;
+        this.possitionY = possitionY;
+        this.direction = direction;
+    }
+    
+    private void checkPosition(int x, int y)
+    {
+        if(gm.pixelArray[x][y]==0)
+        {
+            possitionX = x;
+            possitionY = y;
+        }
+        else
+        {
+             Random randomGenerator = new Random();
+             int randomInt = randomGenerator.nextInt(4);
+             direction = rotate[randomInt];
+             
+        }
+        
     }
     
     public void run()
     {
-        
+        int x,y;
+        while(true)
+        {
+            if(gm.pixelArray[possitionX][possitionY]==0)
+            {
+                break;
+            }
+            
+            if(direction.contentEquals("up"))
+            {
+                x=possitionX;
+                y=possitionY-1;
+                checkPosition(x, y);
+                
+            }
+            else if(direction.contentEquals("down"))
+            {
+                x=possitionX;
+                y=possitionY+1;
+                checkPosition(x, y);
+            }
+            else if(direction.contentEquals("left"))
+            {
+                x=possitionX-1;
+                y=possitionY;
+                checkPosition(x, y);
+            }
+            else if(direction.contentEquals("right"))
+            {
+                x=possitionX+1;
+                y=possitionY;
+                checkPosition(x, y);
+            }
+        }
     }
     
 }
